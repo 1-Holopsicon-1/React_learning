eff5294d5b2e914b752b61c80d71b5d2e41e4cee
1-Holopsicon-1 <mfnbrx@gmail.com>
2021-03-13 00:22:20 +0300

    prealpha

---
+++ content.js
@@ -0,0 +1,28 @@
+
+
+let cards = [{
+    name: "morning",
+    temp: -20,
+    chance: 80,
+    icon: './img/morning.svg',
+  },
+
+  {
+    name: "day",
+    temp: -25,
+    chance: 25,
+    icon: './img/day.svg  '
+  },
+  {
+      name: "evening",
+      temp: -30,
+      chance: 40,
+      icon: './img/evening.svg'
+  },
+  {
+    name: "night",
+    temp: -40,
+    chance: 75,
+    icon: './img/night.svg'
+  }
+];
