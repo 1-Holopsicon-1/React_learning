7e9757294ffcfa4f7ccf91ed370b39411d90c357
1-Holopsicon-1 <mfnbrx@gmail.com>
2021-03-13 00:49:49 +0300

    now it is beatefull(so funny(bc no))

--- content.js
+++ content.js
@@ -1,28 +1,42 @@
+const getNumber = function(max = 11, min = 0) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
+const getRGB = function(a = 1) {
+  let r = getNumber(255);
+  let g = getNumber(255);
+  let b = getNumber(255);
+  return `rgba(${r},${g},${b},${a})`;
+}
 
 let cards = [{
-    name: "morning",
+    name: "Morning",
     temp: -20,
     chance: 80,
     icon: './img/morning.svg',
+    color: getRGB(.2)
   },
 
   {
-    name: "day",
+    name: "Day",
     temp: -25,
     chance: 25,
-    icon: './img/day.svg  '
+    icon: './img/day.svg  ',
+    color: getRGB(.2)
   },
   {
-      name: "evening",
-      temp: -30,
-      chance: 40,
-      icon: './img/evening.svg'
+    name: "Evening",
+    temp: -30,
+    chance: 40,
+    icon: './img/evening.svg',
+    color: getRGB(.2)
+
   },
   {
-    name: "night",
+    name: "Night",
     temp: -40,
     chance: 75,
-    icon: './img/night.svg'
+    icon: './img/night.svg',
+    color: getRGB(.2)
   }
 ];
